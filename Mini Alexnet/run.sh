f685a1454fc129f29540d6afe832bb4695c82c90
Charles H Martin <charlesmartin14@gmail.com>
2018-01-27 19:10:12 -0800

    added seq runs

--- Mini Alexnet/run.sh
+++ Mini Alexnet/run.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 source activate py27
-./cifar10_alexnet.py  > cifar10_alexnet.out
-./cifar10_alexnet.py --random 100 > cifar10_alexnet.r100.out
+
+#./cifar10_alexnet.py  > cifar10_alexnet.out
+#./cifar10_alexnet.py --random 100 > cifar10_alexnet.r100.out
 
 
-./cifar10_alexnet.py --batch_size 2   > cifar10_alexnet.b2.out
-./cifar10_alexnet.py --batch_size 2  --random 100 > cifar10_alexnet.b2.r100.out
+#./cifar10_alexnet.py --batch_size 2   > cifar10_alexnet.b2.out
+##./cifar10_alexnet.py --batch_size 2  --random 100 > cifar10_alexnet.b2.r100.out
+
+for id in `seq 1 25`
+do
+    ./cifar10_alexnet.py --id $id --batch_size 2   > cifar10_alexnet.id$id.b2.out
+    ./cifar10_alexnet.py --id $id --batch_size 2  --random 100 > cifar10_alexnet.id$id.b2.r100.out
+    ./cifar10_alexnet.py --id $id --regularize True > cifar10_alexnet.id$id.r100.wd.out
+done
+
+
+
