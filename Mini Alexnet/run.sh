1327793fdd2ec23d21c964ec157e59e2c2c81bc0
Charles H Martin <charlesmartin14@gmail.com>
2018-04-10 11:42:12 -0700

    bug fix

--- Mini Alexnet/run.sh
+++ Mini Alexnet/run.sh
@@ -2,19 +2,20 @@
 
 source activate py27
 
-#./cifar10_alexnet.py  > cifar10_alexnet.out
-#./cifar10_alexnet.py --random 100 > cifar10_alexnet.r100.out
 
+#./cifar10_alexnet.py --save True  > cifar10_alexnet.out
+./cifar10_alexnet.py --save True --regularize True  > cifar10_alexnet.wd.out
+#./cifar10_alexnet.py --save True --random 100 > cifar10_alexnet.r100.out
+./cifar10_alexnet.py --save True --random 100 --regularize True > cifar10_alexnet.r100.wd.out
 
-#./cifar10_alexnet.py --batch_size 2   > cifar10_alexnet.b2.out
-##./cifar10_alexnet.py --batch_size 2  --random 100 > cifar10_alexnet.b2.r100.out
-
-for id in `seq 1 25`
+for id in `seq 1 10`
 do
-    ./cifar10_alexnet.py --id $id --batch_size 2   > cifar10_alexnet.id$id.b2.out
-    ./cifar10_alexnet.py --id $id --batch_size 100   > cifar10_alexnet.id$id.b100.out
-    ./cifar10_alexnet.py --id $id --batch_size 2  --random 100 > cifar10_alexnet.id$id.b2.r100.out
-    ./cifar10_alexnet.py --id $id --regularize True > cifar10_alexnet.id$id.r100.wd.out
+    ./cifar10_alexnet.py --num_epochs 60  --id $id  --regularize True > cifar10_alexnet.id$id.wd.out
+    ./cifar10_alexnet.py --num_epochs 60  --id $id  --random 100 > cifar10_alexnet.id$id.r100.out
+    for bs in 2 16 32 50 100 150 250 500 1000
+    do
+	./cifar10_alexnet.py --num_epochs 60  --id $id --batch_size $bs > cifar10_alexnet.id$id.b$bs.out
+    done
 done
 
 
