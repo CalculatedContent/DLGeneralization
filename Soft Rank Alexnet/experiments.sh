29597ff86325145af4945bc7ee24ec3bf1b90a13
Christian Clough <christian@cclough-mba.local>
2018-07-01 21:16:14 -0700

    fixes

--- Soft Rank Alexnet/experiments.sh
+++ Soft Rank Alexnet/experiments.sh
@@ -1,4 +1,4 @@
-# python softrank_alexnet.py --softrank_k = 0.1 --batch_size 500
+# python softrank_alexnet.py --softrank_k 0.1 --batch_size 500
 python softrank_alexnet.py --softrank_k 1 --batch_size 500
 python softrank_alexnet.py --softrank_k 0.1 --batch_size 16
 python softrank_alexnet.py --softrank_k 1 --batch_size 16
