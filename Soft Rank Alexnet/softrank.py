203ed97712d3f58af620dead11c4d248875373d0
Christian Clough <christian@cclough-mba.local>
2018-07-01 18:27:57 -0700

    add transpose like TF compatible version of eigenValue decay regularizer https://github.com/keras-team/keras/commit/1c630c3e3c8969b40a47d07b9f2edda50ec69720

--- Soft Rank Alexnet/softrank.py
+++ Soft Rank Alexnet/softrank.py
@@ -7,8 +7,11 @@ import tensorflow as tf
 class SoftRankRegularizer(Regularizer):
     """This class implements the SoftRank regularizer.
     
+    Uses TF compatible syntax from now-deprecated EigenValueDecay regularizer in keras: https://github.com/keras-team/keras/commit/1c630c3e3c8969b40a47d07b9f2edda50ec69720
+    
     Args:
         The constant that controls the regularization on the current layer
+
     Returns:
         The regularized loss (for the training data) and
         the original loss (for the validation data).
@@ -37,15 +40,14 @@ class SoftRankRegularizer(Regularizer):
             domin_eigenvect = K.dot(WW, domin_eigenvect)    
         
         WWd = K.dot(WW, domin_eigenvect)
-        domin_eigenval = K.dot(WWd, domin_eigenvect) / K.dot(domin_eigenvect, domin_eigenvect)  # the corresponding dominant eigenvalue
+        domin_eigenval = K.dot(K.transpose(WWd), domin_eigenvect) / K.dot(K.transpose(domin_eigenvect), domin_eigenvect)  # the corresponding dominant eigenvalue
         
         # Compute variance
-        variance = tf.reduce_sum(tf.square(WW), keepdims=True) / tf.size(WW,out_type=tf.float32)
+        variance = tf.reduce_sum(tf.square(WW), keepdims=True) / tf.size(WW, out_type=tf.float32)
 
         regularization = (variance/domin_eigenval) * self.k 
         return K.sum(regularization)
     
-
     def get_config(self):
         return {"name": self.__class__.__name__,
                 "k": self.k}
