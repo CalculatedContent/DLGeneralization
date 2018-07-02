0512dc7f207fc279efafe50c76f682c071e7b949
Christian Clough <christian@cclough-mba.local>
2018-07-01 17:41:21 -0700

    fixes

--- Soft Rank Alexnet/softrank.py
+++ Soft Rank Alexnet/softrank.py
@@ -1,6 +1,8 @@
 import numpy as np
 from keras import backend as K
 from keras.regularizers import Regularizer
+from keras.layers import Reshape
+import tensorflow as tf
 
 class SoftRankRegularizer(Regularizer):
     """This class implements the SoftRank regularizer.
@@ -25,10 +27,10 @@ class SoftRankRegularizer(Regularizer):
 
         # import pdb
         # pdb.set_trace()
-        
+
         # Reshape W to 2D, combining 3 smallest dims
         W_shape_sort = sorted(W.shape)
-        W_rshp = tf.reshape(W, (W_shape_sort[0]*W_shape_sort[1]*W_shape_sort[2],W.shape_sort[3]))
+        W_rshp = Reshape(W, (W_shape_sort[0]*W_shape_sort[1]*W_shape_sort[2],W.shape_sort[3]))
 
         WW = K.dot(K.transpose(W_rshp), W_rshp)
         dim1, dim2 = K.eval(K.shape(WW))
